b2ebee94eb1474e68e592e8fdb39eb5ede0a646b
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2023-12-20 05:22:14 +0800

    Protocol extension for Number
    - In-past? and In-future? for numbers, considered as unix timestamps
    - Starting to remove joda time which is deprecated

--- src/detijd/predicates.clj
+++ src/detijd/predicates.clj
@@ -1,6 +1,5 @@
 (ns detijd.predicates
-  (:require [clj-time.core :as t]
-            [detijd.core :refer [week-number]])
+  (:require [detijd.core :refer [week-number]])
   (:import [java.time Instant LocalDate LocalDateTime ZoneId Period]
            [java.time.temporal ChronoUnit]
            [org.joda.time DateTime]))
@@ -23,38 +22,17 @@
 
 (extend-protocol DeTijd
 
-  DateTime
+  Number
 
-  (last-days? [d x]
-    (t/within? (t/interval (t/minus (t/now) (t/days x)) (t/now)) d))
-  (last-hours? [d x]
-    (t/within? (t/interval (t/minus (t/now) (t/hours x)) (t/now)) d))
-  (last-minutes? [d x]
-    (t/within? (t/interval (t/minus (t/now) (t/minutes x)) (t/now)) d))
-  (last-months? [d x]
-    (t/within? (t/interval (t/minus (t/now) (t/months x)) (t/now)) d))
-  (today? [d] (= (.toLocalDate d) (t/today)))
-  (same-week-number? [d] (= (.getWeekOfWeekyear d) (.getWeekOfWeekyear (t/today))))
-  (same-year? [d] (= (.getYear d) (.getYear (LocalDate/now))))
-  (in-future? [d]
-    (t/before? (t/now) d))
   (in-past? [d]
-    (t/after? (t/now) d))
-  (days-elapsed [d] (if (in-past? d)
-                       (t/in-days (t/interval d (t/now)))
-                       (throw (AssertionError. "Date is not in past"))))
-  (days-remain [d] (if (in-past? d)
-                       (t/in-days (t/interval (t/now) d))
-                       (throw (AssertionError. "Date is not in past"))))
-  (seconds-elapsed [d] (if (in-past? d)
-                         (t/in-seconds (t/interval d (t/now)))
-                         (throw (AssertionError. "Date is not in past"))))
-  (hours-elapsed [d] (if (in-past? d)
-                         (t/in-hours (t/interval d (t/now)))
-                         (throw (AssertionError. "Date is not in past"))))
-  (minutes-elapsed [d] (if (in-past? d)
-                         (t/in-minutes (t/interval d (t/now)))
-                         (throw (AssertionError. "Date is not in past"))))
+    (condp = (count (str d))
+      10 (> (.getEpochSecond (Instant/now)) d)
+      13 (> (System/currentTimeMillis) d)))
+
+  (in-future? [d]
+    (condp = (count (str d))
+      10 (< (.getEpochSecond (Instant/now)) d)
+      13 (< (System/currentTimeMillis) d)))
 
   Instant
 
